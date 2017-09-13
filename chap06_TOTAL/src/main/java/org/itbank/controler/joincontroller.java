e249e33cd67406262de5ed1b6712b9d1d754daf1
1103-30 <1103-30@1103-30-PC>
2017-09-13 14:45:40 +0900

    test

--- chap06_TOTAL/src/main/java/org/itbank/controler/joincontroller.java
+++ chap06_TOTAL/src/main/java/org/itbank/controler/joincontroller.java
@@ -1,5 +1,44 @@
 package org.itbank.controler;
 
-public class joincontroller {
+import java.sql.SQLException;
+import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import org.itbank.controler.MemberDaoMyBastis;
+
+@Controller
+public class joincontroller {
+	@Autowired
+	MemberDaoMyBastis memberDao;
+
+	@GetMapping(path = "/join")
+	public String joinGetHandle() {
+		return "t_join";
+	}
+
+	@PostMapping("/join")
+	public String joinPostHandle(@RequestParam Map map, HttpSession session, Model model) {
+		try {
+			boolean b = memberDao.addOne(map);
+			session.setAttribute("auth", "on");
+			return "redirect:/";
+		} catch (Exception e) {
+			e.printStackTrace();
+			model.addAttribute("temp", map);
+			return "join";
+		}
+	}
+
+	@PostMapping("/signup_check/{mode}")
+	public String signupHandle() {
+		return "";
+	}
 }
